65a6f488552421fef871c6b88d7f26cbaec44a92
mac <ha_wang@coloradocollege.edu>
2020-05-14 14:21:49 -0600

    added back button and rejected text edition for instruction page

--- TetrisFinalProject/src/sample/InstructionsPageController.java
+++ TetrisFinalProject/src/sample/InstructionsPageController.java
@@ -1,9 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,5 +24,14 @@ public class InstructionsPageController implements Initializable {
                 "Right arrow key: move block to the right\n" + "\n" +
                 "Down arrow key: move block down\n" + "\n" +
                 "Up arrow key: rotate block clockwise");
+    }
+
+    @FXML
+    public void BackToMenu(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
     }
 }
