07b5e872b84e5fc3bdb211a0aeef9c0acec08ab0
mac <ha_wang@coloradocollege.edu>
2020-05-15 10:36:12 -0600

    newest

--- TetrisFinalProject/src/sample/Tetris.java
+++ TetrisFinalProject/src/sample/Tetris.java
@@ -50,13 +50,14 @@ public class Tetris extends Application implements Initializable {
     public int level = 1;
     public int RowsRemoved = 0;
     public int FallingSpeed = 500;
+    public int top = 0;
     Text over;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         placeholder = new int[X_max / size][Y_max / size];
         Level.setText("Level: " + level);
-        a = Controller.MakeForm();
+        a = MakeForm();
         for (int[] a : placeholder) {
             Arrays.fill(a, 0);
         }
@@ -76,9 +77,9 @@ public class Tetris extends Application implements Initializable {
     public void moveOnKeyPress(KeyEvent event) {
         if (game) {
             if (event.getCode() == KeyCode.LEFT) {
-                Controller.MoveLeft(a);
+                MoveLeft(a);
             } else if (event.getCode() == KeyCode.RIGHT) {
-                Controller.MoveRight(a);
+                MoveRight(a);
             } else if (event.getCode() == KeyCode.DOWN) {
                 MoveDown(a);
             } else if (event.getCode() == KeyCode.UP) {
@@ -119,7 +120,7 @@ public class Tetris extends Application implements Initializable {
             placeholder[(int) (form.b.getX() / size)][(int) (form.b.getY() / size)] = 1;
             placeholder[(int) (form.c.getX() / size)][(int) (form.c.getY() / size)] = 1;
             placeholder[(int) (form.d.getX() / size)][(int) (form.d.getY() / size)] = 1;
-            a = Controller.MakeForm();
+            a = MakeForm();
             RemoveRows(holder);
             if (a == null) {
                 GameOver();
@@ -129,7 +130,7 @@ public class Tetris extends Application implements Initializable {
 
         }
 
-        if (form.a.getY() + move < Y_max && form.b.getY() + move < Y_max && form.c.getY() + move < Y_max && form.d.getY() + move < Y_max) {
+       else if (form.a.getY() + move < Y_max && form.b.getY() + move < Y_max && form.c.getY() + move < Y_max && form.d.getY() + move < Y_max) {
             int movea = placeholder[(int) (form.a.getX() / size)][(int) ((form.a.getY() / size) + 1)];
             int moveb = placeholder[(int) (form.b.getX() / size)][(int) ((form.b.getY() / size) + 1)];
             int movec = placeholder[(int) (form.c.getX() / size)][(int) ((form.c.getY() / size) + 1)];
@@ -500,7 +501,11 @@ public class Tetris extends Application implements Initializable {
                 }
                 for (Node node : Rectangles) {
                     Rectangle a = (Rectangle) node;
-                    placeholder[(int) a.getX() / size][(int) a.getY() / size] = 1;
+                    try {
+                        placeholder[(int) a.getX() / size][(int) a.getY() / size] = 1;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+
+                    }
 
                 }
                 Rectangles.clear();
@@ -537,10 +542,10 @@ public class Tetris extends Application implements Initializable {
     }
 
     public boolean CheckLegalMoveDown(Form form) {
-        boolean CheckRectA = placeholder[(int) form.a.getX() / size][(int) form.a.getY() / size + 1] == 1;
-        boolean CheckRectB = placeholder[(int) form.b.getX() / size][(int) form.b.getY() / size + 1] == 1;
-        boolean CheckRectC = placeholder[(int) form.c.getX() / size][(int) form.c.getY() / size + 1] == 1;
-        boolean CheckRectD = placeholder[(int) form.d.getX() / size][(int) form.d.getY() / size + 1] == 1;
+        boolean CheckRectA = placeholder[(int) form.a.getX() / size][(int) (form.a.getY() / size) + 1] == 1;
+        boolean CheckRectB = placeholder[(int) form.b.getX() / size][(int) (form.b.getY() / size) + 1] == 1;
+        boolean CheckRectC = placeholder[(int) form.c.getX() / size][(int) (form.c.getY() / size) + 1] == 1;
+        boolean CheckRectD = placeholder[(int) form.d.getX() / size][(int) (form.d.getY() / size) + 1] == 1;
         return CheckRectA || CheckRectB || CheckRectC || CheckRectD;
     }
 
@@ -618,5 +623,160 @@ public class Tetris extends Application implements Initializable {
         window.show();
     }
 
+    public static void MoveLeft(Form form) {
+        if (form.a.getX() - move >= 0 && form.b.getX() - move >= 0 && form.c.getX() - move >= 0 && form.d.getX() - move >= 0) {
+            int movea = placeholder[(int) ((form.a.getX() / size) - 1)][(int) (form.a.getY() / size)];
+            int moveb = placeholder[(int) ((form.b.getX() / size) - 1)][(int) (form.b.getY() / size)];
+            int movec = placeholder[(int) ((form.c.getX() / size) - 1)][(int) (form.c.getY() / size)];
+            int moved = placeholder[(int) ((form.d.getX() / size) - 1)][(int) (form.d.getY() / size)];
+            if (movea == 0 && moveb == 0 && movec == 0 && moved == 0) {
+                form.a.setX(form.a.getX() - move);
+                form.b.setX(form.b.getX() - move);
+                form.c.setX(form.c.getX() - move);
+                form.d.setX(form.d.getX() - move);
+            }
+        }
+    }
 
+    public static void MoveRight(Form form) {
+        if (form.a.getX() + move <= X_max - size && form.b.getX() + move <= X_max - size
+                && form.c.getX() + move <= X_max - size && form.d.getX() + move <= X_max - size) {
+            int movea = placeholder[(int) ((form.a.getX() / size) + 1)][(int) (form.a.getY() / size)];
+            int moveb = placeholder[(int) ((form.b.getX() / size) + 1)][(int) (form.b.getY() / size)];
+            int movec = placeholder[(int) ((form.c.getX() / size) + 1)][(int) (form.c.getY() / size)];
+            int moved = placeholder[(int) ((form.d.getX() / size) + 1)][(int) (form.d.getY() / size)];
+            if (movea == 0 && moveb == 0 && movec == 0 && moved == 0) {
+                form.a.setX(form.a.getX() + move);
+                form.b.setX(form.b.getX() + move);
+                form.c.setX(form.c.getX() + move);
+                form.d.setX(form.d.getX() + move);
+            }
+        }
+    }
+
+    public static Form MakeForm() {
+        int DecideForm = (int) (Math.random() * 100);
+        if (!Checker(DecideForm)) {
+            return null;
+        }
+        String name;
+        Rectangle a = new Rectangle(size - 1, size - 1);
+        Rectangle b = new Rectangle(size - 1, size - 1);
+        Rectangle c = new Rectangle(size - 1, size - 1);
+        Rectangle d = new Rectangle(size - 1, size - 1);
+        if (DecideForm < 15) {
+            a.setX(X_max / 2 - size);
+            b.setX(X_max / 2);
+            c.setX(X_max / 2 + size);
+            d.setX(X_max / 2 + 2 * size);
+            name = "I";
+        } else if (DecideForm < 30) {
+            a.setX(X_max / 2);
+            b.setX(X_max / 2);
+            b.setY(size);
+            c.setX(X_max / 2 + size);
+            d.setX(X_max / 2 + size);
+            d.setY(size);
+            name = "O";
+        } else if (DecideForm < 45) {
+            a.setX(X_max / 2 - size);
+            b.setX(X_max / 2);
+            c.setX(X_max / 2);
+            c.setY(size);
+            d.setX(X_max / 2 + size);
+            name = "T";
+        } else if (DecideForm < 60) {
+            a.setX(X_max / 2 - size);
+            b.setX(X_max / 2 - size);
+            b.setY(size);
+            c.setX(X_max / 2);
+            c.setY(size);
+            d.setX(X_max / 2 + size);
+            d.setY(size);
+            name = "J";
+        } else if (DecideForm < 75) {
+            a.setX(X_max / 2 + size);
+            b.setX(X_max / 2 + size);
+            b.setY(size);
+            c.setX(X_max / 2);
+            c.setY(size);
+            d.setX(X_max / 2 - size);
+            d.setY(size);
+            name = "L";
+        } else if (DecideForm < 90) {
+            a.setX(X_max / 2 + size);
+            b.setX(X_max / 2);
+            c.setX(X_max / 2);
+            c.setY(size);
+            d.setX(X_max / 2 - size);
+            d.setY(size);
+            name = "S";
+        } else {
+            a.setX(X_max / 2 - size);
+            b.setX(X_max / 2);
+            c.setX(X_max / 2);
+            c.setY(size);
+            d.setX(X_max / 2 + size);
+            d.setY(size);
+            name = "Z";
+        }
+
+        return new Form(a, b, c, d, name);
+    }
+
+    public static boolean Checker(int FormNumber) {
+        boolean result = true;
+        if (FormNumber < 15) {
+            if (placeholder[(X_max / (2 * size)) - 1][0] == 1
+                    || placeholder[X_max / (2 * size)][0] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][0] == 1
+                    || placeholder[(X_max / (2 * size)) + 2][0] == 1) {
+                result = false;
+            }
+        } else if (FormNumber < 30) {
+            if (placeholder[X_max / (2 * size)][0] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][0] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][1] == 1) {
+                result = false;
+            }
+        } else if (FormNumber < 45) {
+            if (placeholder[(X_max / (2 * size)) - 1][0] == 1
+                    || placeholder[X_max / (2 * size)][0] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][0] == 1) {
+                result = false;
+            }
+        } else if (FormNumber < 60) {
+            if (placeholder[(X_max / (2 * size)) - 1][0] == 1
+                    || placeholder[(X_max / (2 * size)) - 1][1] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][1] == 1) {
+                result = false;
+            }
+        } else if (FormNumber < 75) {
+            if (placeholder[(X_max / (2 * size)) + 1][0] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][1] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) - 1][1] == 1) {
+                result = false;
+            }
+        } else if (FormNumber < 90) {
+            if (placeholder[(X_max / (2 * size)) + 1][0] == 1
+                    || placeholder[X_max / (2 * size)][0] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) - 1][1] == 1) {
+                result = false;
+            }
+        } else {
+            if (placeholder[(X_max / (2 * size)) - 1][0] == 1
+                    || placeholder[X_max / (2 * size)][0] == 1
+                    || placeholder[X_max / (2 * size)][1] == 1
+                    || placeholder[(X_max / (2 * size)) + 1][1] == 1) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
 }
